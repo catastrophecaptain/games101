044d8009ff6a0bf3e146d50d2f73a97d1f8a4639
_klein_ <_352641075@qq.com_>
2024-02-03 18:05:41 +0800

    work0 completed

--- work0/main.cpp
+++ work0/main.cpp
@@ -1,24 +1,25 @@
-//#include<cmath>
-#include<Eigen/Core>
-#include<Eigen/Dense>
-#include<iostream>
+#include <cmath>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <iostream>
 
-int main(){
+int main()
+{
 
     // Basic Example of cpp
     std::cout << "Example of cpp \n";
     float a = 1.0, b = 2.0;
     std::cout << a << std::endl;
-    std::cout << a/b << std::endl;
+    std::cout << a / b << std::endl;
     std::cout << std::sqrt(b) << std::endl;
     std::cout << std::acos(-1) << std::endl;
-    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
+    std::cout << std::sin(30.0 / 180.0 * acos(-1)) << std::endl;
 
     // Example of vector
     std::cout << "Example of vector \n";
     // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
+    Eigen::Vector3f v(1.0f, 2.0f, 3.0f);
+    Eigen::Vector3f w(1.0f, 0.0f, 0.0f);
     // vector output
     std::cout << "Example of output \n";
     std::cout << v << std::endl;
@@ -33,7 +34,7 @@ int main(){
     // Example of matrix
     std::cout << "Example of matrix \n";
     // matrix definition
-    Eigen::Matrix3f i,j;
+    Eigen::Matrix3f i, j;
     i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
     j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
     // matrix output
@@ -44,12 +45,16 @@ int main(){
     // matrix multiply i * j
     // matrix multiply vector i * v
 
-
-    /* 
-    * PA 0
-    */
+    /*
+     * PA 0
+     */
     // TO DO: Define point P
     // TO DO: Define rotation matrix M
     // TO DO: M * P
+    Eigen::Vector3f P{2.0, 1.0, 1.0};
+    Eigen::Matrix3f M;
+    M << cos(M_PI / 4), sin(M_PI / 4), 0.0, -sin(M_PI / 4), cos(M_PI / 4), 0.0, 0.0, 0.0, 1.0;
+
+    std::cout << M * P << std::endl;
     return 0;
 }
